c24f8f2927ec3f4d6aa0a1e4637d53ed79aa630b
Patom Thirawetchayan <patomt@gmail.com>
2022-02-16 15:32:16 -0500

    basic Developer CRUD

--- GameFinder.Services/DeveloperService.cs
+++ GameFinder.Services/DeveloperService.cs
@@ -12,15 +12,72 @@ namespace GameFinder.Services
     {
         public bool CreateDeveloper(DeveloperCreate model)
         {
+            var entity = new Developer()
+            {
+                Name = model.Name,
+                Description = model.Description,
+                Link = model.Link
+            };
+
             using (var ctx = new ApplicationDbContext())
             {
-                var entity = new Developer()
-                {
-                    Name = model.Name,
-                    Description = model.Description,
-                    Link = model.Link
-                };
                 ctx.Developers.Add(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<DeveloperListItem> GetDevelopers()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Developers.Select(d => new DeveloperListItem()
+                {
+                    DeveloperId = d.DeveloperId,
+                    Name = d.Name,
+                });
+
+                return query.ToArray();
+            }
+        }
+
+        public DeveloperDetail GetDeveloperById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var dev = ctx.Developers.Find(id);
+
+                return new DeveloperDetail()
+                {
+                    DeveloperId = dev.DeveloperId,
+                    Name = dev.Name,
+                    Description = dev.Description,
+                    Link = dev.Link
+                };
+            }
+        }
+
+        public bool UpdateDeveloper(int id, DeveloperUpdate model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var dev = ctx.Developers.Find(id);
+
+                dev.Name = model.Name;
+                dev.Description = model.Description;
+                dev.Link = model.Link;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteDeveloper(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var dev = ctx.Developers.Find(id);
+
+                ctx.Developers.Remove(dev);
 
                 return ctx.SaveChanges() == 1;
             }
