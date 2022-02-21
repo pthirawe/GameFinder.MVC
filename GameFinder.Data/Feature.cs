41933a4cc641c3cb79a79135ab477c623f056bd0
Patom Thirawetchayan <patomt@gmail.com>
2022-02-21 14:23:27 -0500

    added Feature CRUD, service, controller, and views

---
+++ GameFinder.Data/Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFinder.Data
+{
+    public class Feature
+    {
+        [Key]
+        public int FeatureId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ GameFinder.Services/FeatureService.cs
@@ -0,0 +1,69 @@
+﻿using GameFinder.Data;
+using GameFinder.Models.FeatureModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFinder.Services
+{
+    public class FeatureService
+    {
+        public bool CreateFeature(FeatureCreate model)
+        {
+            var entity = new Genre()
+            {
+                Name = model.Name,
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Genres.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<Feature> GetFeatures()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                return ctx.Features.ToList();
+            }
+        }
+
+        public Feature GetFeatureById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Features.Find(id);
+
+                return entity;
+            }
+        }
+
+        public bool FeatureUpdate(int id, FeatureUpdate model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Features.Find(id);
+                
+                entity.Name = model.Name;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool FeatureDelete(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Features.Find(id);
+
+                ctx.Features.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
