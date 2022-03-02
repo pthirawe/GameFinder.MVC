f9bc19800d9a2ff9175ddb97e9fbb46730bedffc
Patom Thirawetchayan <patomt@gmail.com>
2022-03-02 13:55:39 -0500

    implemented filtering

--- GameFinder.MVC/Controllers/GameController.cs
+++ GameFinder.MVC/Controllers/GameController.cs
@@ -2,6 +2,7 @@
 using GameFinder.Services;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,10 +13,22 @@ namespace GameFinder.MVC.Controllers
     public class GameController : Controller
     {
         // GET: Game
-        public ActionResult Index()
+        public ActionResult Index(string title, string genre, string dev, string pub)
         {
             var svc = CreateService();
             var games = svc.GetGames();
+
+            if(!String.IsNullOrEmpty(title))
+                games = games.Where(g => g.Title.ToLower().Contains(title.ToLower()));
+
+            if (!String.IsNullOrEmpty(genre))
+                games = games.Where(g => g.Genre.Any(i=> i.ToLower().Contains(genre.ToLower())));
+
+            if (!String.IsNullOrEmpty(dev))
+                games = games.Where(g => g.Developer.ToLower().Contains(dev.ToLower()));
+
+            if (!String.IsNullOrEmpty(pub))
+                games = games.Where(g => g.Publisher.ToLower().Contains(pub.ToLower()));
 
             return View(games);
         }
@@ -48,7 +61,51 @@ namespace GameFinder.MVC.Controllers
 
             return View(model);
         }
+        /*  Replaced by filters in index
+        // GET: Game/Search
+        public ActionResult Search()
+        {
+            dynamic query = new ExpandoObject();
+            query.Search = new GameSearch();
+            query.Results = new List<GameListItem>();
 
+            return View();
+        }
+
+        // POST: Game/Search
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Search(GameSearch query)
+        {
+            bool executed = false;
+            var results = new List<GameListItem>();
+            var svc = CreateService();
+
+            if (query.Title != null)
+            {
+                results.AddRange(svc.GetGamesByTitle(query.Title));
+                executed = true;
+            }
+            if (query.Developer != null)
+            {
+                results.AddRange(svc.GetGamesByDev(query.Developer));
+                executed = true;
+            }
+            if (query.Publisher != null)
+            {
+                results.AddRange(svc.GetGamesByPublisher(query.Publisher));
+                executed = true;
+            }
+            if (executed)
+            {
+                return RedirectToAction("SearchResults", "Game", results);
+            }
+
+
+            ModelState.AddModelError("", "Search terms are empty.");
+            return View();
+        }
+        */
         // GET: Game/Edit/ID
         public ActionResult Edit(int id)
         {
--- GameFinder.Models/GameSearch.cs
+++ GameFinder.Models/GameSearch.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 
 namespace GameFinder.Models
 {
-    public class GameListItem
+    public class GameSearch
     {
-        public int GameId { get; set; }
-        public string Title { get; set; } 
-        public List<string> Genre { get; set; }
+        public string Title { get; set; }
         public string Developer { get; set; }
         public string Publisher { get; set; }
     }
